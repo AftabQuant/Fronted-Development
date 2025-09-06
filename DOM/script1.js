0a700ba4d4397ba123584de058f44e27a02cc0c1
Md Aftab Uddin <iamaftabuddin123@gmail.com>
2025-09-06 23:06:48 +0530

    DOM Start

--- DOM/script1.js
+++ DOM/script1.js
@@ -2,3 +2,23 @@ console.log("Hello i am from javascript file.");
 
 let x = document.querySelectorAll(".yes");
 console.log(x);
+
+// Change CSS
+let y = document.querySelector("h2")
+console.log(y)
+y.style.color = "red";
+y.style.fontFamily = "Gill Sans";
+
+// Change HTML
+y.innerHTML = "Hello changed."
+
+// Even Listener
+setTimeout(function() {
+    y.innerHTML = "Print the hello after 5 sec.";
+}, 5*1000);
+
+
+setTimeout(function() {
+    y.style.color = "blue";
+    y.style.backgroundColor = "yellow";
+}, 5*1000);
